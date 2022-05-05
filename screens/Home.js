38fb6f5e712c8c6ce31edbced24d58489a30122b
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-05-05 14:40:39 +0545

    feat: search function

--- screens/Home.js
+++ screens/Home.js
@@ -5,17 +5,37 @@ import { COLORS, NFTData } from '../constants'
 import { HomeHeader, FocusedStatusBar, NFTCard, RectButton  } from '../components'
 
 const Home = () => {
+    const [nftData, setNftData] = useState(NFTData)
+
+    const handleSearch = (value) => {
+        console.warn(value)
+
+        if (!value.length) return setNftData(NFTData)
+    
+        const filteredData = NFTData
+                    .filter(
+                        (item) => item.name.toLowerCase()
+                        .includes(value.toLowerCase())
+                    )
+
+        if(filteredData.length) {
+            setNftData(filteredData)
+        } else {
+            setNftData(NFTData)
+        }
+    }
+
     return (
         <View style={{ flex: 1 }}>
             <FocusedStatusBar backgroundColor='purple' />
             <View style={{ flex: 1 }}>
                 <View style={{ zIndex: 0 }}>
                     <FlatList 
-                        data={NFTData}
+                        data={nftData}
                         renderItem={({ item }) => <NFTCard data={item} />}
                         keyExtractor={(item) => item.id}
                         showsVerticalScrollIndicator={false}
-                        ListHeaderComponent={<HomeHeader />}
+                        ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
                     />
                 </View>
                 <View style={{
